859674a94756a6e3e84f3f83dc87c663f99b0a9f
Kraften <johannes.kraft1@gmail.com>
2022-02-22 18:26:06 +0100

    wow

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -2,19 +2,19 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 import { getFirestore } from 'firebase/firestore';
+import 'dotenv/config';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCF5Abb2EMtI-yqa4izpADm8RqyLiLbkrs',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: 'calendar-92ec5.firebaseapp.com',
-    databaseURL:
-        'https://calendar-92ec5-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'calendar-92ec5',
+    databaseURL: process.env.REACT_APP_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: 'calendar-92ec5.appspot.com',
     messagingSenderId: '1006124787273',
     appId: '1:1006124787273:web:4361d8a41043cc8bc84198',
     measurementId: 'G-MEBDZXZCG5'
 };
-
+console.log(process.env);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const dbFirestore = getFirestore();
