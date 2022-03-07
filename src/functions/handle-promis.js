2e656c867c34f0eefcf72aa6586047ead28d82ee
Kraften <johannes.kraft1@gmail.com>
2022-03-07 15:08:41 +0100

    changed structure of utils folder
    changed name of utils foler to functions
    
    extracted utils functions to seperate files

--- src/functions/handle-promis.js
+++ src/functions/handle-promis.js
@@ -1,15 +1,4 @@
 /**
- * Viewport fix for Chrome and Safari on iOS.
- * @param  {} innerHeight height of users window (send window.innerHeight).
- */
-const viewPortFix = (innerHeight) => {
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-};
-
-/**
  * @description ### Returns Go / Lua like responses(data, err)
  * when used with await
  *
@@ -36,4 +25,4 @@ const handlePromise = (promise) => {
         .catch((error) => Promise.resolve([undefined, error]));
 };
 
-export { viewPortFix, handlePromise };
+export { handlePromise };
