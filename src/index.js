61609bc3fd6f380564e56f92f08e44c680da2be4
Kraften <johannes.kraft1@gmail.com>
2022-10-13 19:08:47 +0200

    React update 17 --> 18

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './store/auth-context';
 
-
-ReactDOM.render(
-  <AuthContextProvider>
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <AuthContextProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-  </AuthContextProvider>,
-  document.getElementById('root')
+    </AuthContextProvider>
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
